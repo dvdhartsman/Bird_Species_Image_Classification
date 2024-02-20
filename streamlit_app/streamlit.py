645260e39c47f7cc44189176b511fdf1ad3a7f2b
David Hartsman <dvdhartsman@gmail.com>
2024-02-20 14:33:28 -0500

    form statement

--- streamlit_app/streamlit.py
+++ streamlit_app/streamlit.py
@@ -145,13 +145,24 @@ def main():
     if st.button("Yes"):
         st.write("Yeah, we thought so...")
     if st.button("No"):
-        user_input = st.text_input("What is the name of the bird you provided?")
-        st.write(user_input)
-        if user_input:
-            if user_input.upper() in classes.values():
-                st.write("The model should've known that, but we made an incorrect prediction")
-            else:
-                st.write("This type of bird is not present in our training data, sorry about that")
+        with st.form(key='my_form'):
+            user_input = st.text_input("What is the name of the bird you provided?")
+            submit_button = st.form_submit_button(label='Submit')
+            if submit_button:
+                st.write(user_input)
+                if user_input:
+                    if user_input.upper() in classes.values():
+                        st.write("The model should've known that, but we made an incorrect prediction")
+                    else:
+                        st.write("This type of bird is not present in our training data, sorry about that")
+#     if st.button("No"):
+#         user_input = st.text_input("What is the name of the bird you provided?")
+#         st.write(user_input)
+#         if user_input:
+#             if user_input.upper() in classes.values():
+#                 st.write("The model should've known that, but we made an incorrect prediction")
+#             else:
+#                 st.write("This type of bird is not present in our training data, sorry about that")
     
 
     github_project_url = "https://github.com/dvdhartsman/Bird_Species_Image_Classification"
